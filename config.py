fcaf6f5c3e48de2fdc31195024480c7b26dae2ce
huge <zhuhu00@foxmail.com>
2021-10-26 20:40:09 +0800

    Try to use git action to auto issue.

---
+++ config.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# Authentication for user filing issue (must have read/write access to repository to add issue to)
+USERNAME = 'zhuhu00'
+TOKEN = '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'
+
+# The repository to add this issue to
+REPO_OWNER = 'zhuhu00'
+REPO_NAME = 'arxiv-daily-notice'
+
+# Set new submission url of subject
+NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
+
+# Keywords to search
+KEYWORD_LIST = ["SLAM","Visual inertial","livox","loam","Visual inertial odometry","lidar","loop detection","autonomous driving","mapping","localization"]
